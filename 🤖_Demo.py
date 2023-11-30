b934b4d47763cab7673f71df10d038f5fb4443d3
Josiah Coad <josiah@coad.net>
2023-11-30 14:04:07 -0600

    bug

--- 🤖_Demo.py
+++ 🤖_Demo.py
@@ -34,7 +34,6 @@ def get_prompts():
 def clickable_image(image_url, target_url, image_size=100):
     markdown = f'<a href="{target_url}" target="_blank"><img src="{image_url}" width="{image_size}" height="{image_size}"></a>'
     st.markdown(markdown, unsafe_allow_html=True)
-    st.image(image_url, width=image_size)
 
 
 async def generate_post(session, business_context, knowledge, language, canvas_name, prompt, topic, cta, intention, caption_length, color_pallete):
